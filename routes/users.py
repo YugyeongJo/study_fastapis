a2029c3c9e280adfeb5a6e1cd7fef8ed0c36384d
Yugyeong Jo <yugyeongjo1215@gmail.com>
2024-01-03 18:12:15 +0900

    add

--- routes/users.py
+++ routes/users.py
@@ -9,8 +9,14 @@ from fastapi import Request
 # html 틀이 있는 폴더 위치
 templates = Jinja2Templates(directory="templates/")
 
-# 회원 가입 / users/inserts
-@router.get("/insert")
+# 회원 가입 form   /users/form  > users/inserts.html
+@router.get("/form")
 async def insert(request:Request):
     pass
     return templates.TemplateResponse(name="users/inserts.html",context = {"request": request})
+
+# 회원 가입 /users/inserts > 가입이 완료되면 로그인으로 이동 users/login.html
+@router.get("/insert")
+async def insert(request:Request):
+    pass    #bisness는 여기(실제 들어오는 곳과 나가는 곳이 다른 곳)
+    return templates.TemplateResponse(name="users/login.html",context = {"request": request})
